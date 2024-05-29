797a71ad2057650e52b8e7c8d0f0abf83eed81bb
belbix <7453635@gmail.com>
2024-05-29 10:05:02 +0300

    change headers2

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ dotenv.config();
 // RUN BOTS
 runBot(process.env.SACRA_LIVING_HERO_BOT, 'Heroes: Living', LIVING_HERO_QUERY, 'heroEntities');
 runBot(process.env.SACRA_DEAD_HERO_BOT, 'Heroes: Dead', DEAD_HERO_QUERY, 'heroEntities');
+runBot(process.env.SACRA_HERO_REINFORCEMENT_BOT, 'Heroes: Stacked', HERO_REINFORCEMENT_QUERY, 'heroEntities');
 runBot(process.env.SACRA_ITEMS_BOT, 'Items: Available', ITEM_QUERY, 'itemEntities');
 runBot(process.env.SACRA_BROKEN_ITEMS_BOT, 'Items: Destroyed', DESTROYED_ITEM_QUERY, 'itemEntities');
 runBot(process.env.SACRA_USER_BOT, 'Unique users', USER_QUERY, 'userEntities');
@@ -14,7 +15,6 @@ runBot(process.env.SACRA_HERO_FINISH_FIRST_BIOME_BOT, 'Biome #1', HERO_FINISH_FI
 runBot(process.env.SACRA_HERO_FINISH_SECOND_BIOME_BOT, 'Biome #2', HERO_FINISH_SECOND_BIOME_QUERY, 'heroEntities', 'Bosses killed');
 runBot(process.env.SACRA_HERO_FINISH_THIRD_BIOME_BOT, 'Biome #3', HERO_FINISH_THIRD_BIOME_QUERY, 'heroEntities', 'Bosses killed');
 runBot(process.env.SACRA_HERO_FINISH_FOURTH_BIOME_BOT, 'Biome #4', HERO_FINISH_FOURTH_BIOME_QUERY, 'heroEntities', 'Bosses killed');
-runBot(process.env.SACRA_HERO_REINFORCEMENT_BOT, 'Stacked heroes', HERO_REINFORCEMENT_QUERY, 'heroEntities');
 runBot(process.env.SACRA_TOTAL_SUPPLY_BOT, 'Total supply', TOKEN_QUERY, 'totalSupply');
 runBot(process.env.SACRA_REWARDS_FIRST_BIOME_BOT, 'Rewards per dung Biome #1', TOKEN_QUERY, 'rewardsPerBiome', '', [1]);
 runBot(process.env.SACRA_REWARDS_SECOND_BIOME_BOT, 'Rewards per dung Biome #2', TOKEN_QUERY, 'rewardsPerBiome', '', [2]);
